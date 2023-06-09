32f32ea74147c706b51b7430135e28cd6df62545
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2023-06-09 16:05:13 +0530

    conf vpc, rds

--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.1.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+  region = "ap-south-1"
 }
